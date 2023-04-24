c097811e30b60ea30f68652b12fd7f5d2783e226
ssamsel <ssamsel@umass.edu>
2023-04-24 15:47:14 -0400

    fixed spacing in time.js

--- app/time.js
+++ app/time.js
@@ -21,18 +21,18 @@ export class Timestamp {
         });
 
         if (diff[0] > 0){
-           return diff[0].toString() + "year" + this._plural(diff[0]) + "ago";
+           return diff[0].toString() + " year" + this._plural(diff[0]) + "ago";
         }
         if (diff[1] > 0){
-           return diff[1].toString() + "month" + this._plural(diff[1]) + "ago";
+           return diff[1].toString() + " month" + this._plural(diff[1]) + "ago";
         }
         if (diff[2] > 0){
-           return diff[2].toString() + "day" + this._plural(diff[2]) + "ago";
+           return diff[2].toString() + " day" + this._plural(diff[2]) + "ago";
         }
         if (diff[3] > 0){
-           return diff[3].toString() + "hour" + this._plural(diff[3]) + "ago";
+           return diff[3].toString() + " hour" + this._plural(diff[3]) + "ago";
         }
-        return diff[4].toString() + "minute" + this._plural(diff[3]) + "ago";
+        return diff[4].toString() + " minute" + this._plural(diff[3]) + "ago";
     }
 
     _plural(num){
