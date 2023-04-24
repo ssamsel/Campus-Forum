739765f67b69157d6ccbabc5536758d9507e313b
Nithin Joshy <njoshy@umass.edu>
2023-04-24 15:50:59 -0400

    added crud method to fetch post info

--- app/client/js/crud.js
+++ app/client/js/crud.js
@@ -34,6 +34,14 @@ export async function createThread(username, hash, post) {
     return data;
 }
 
+export async function getThread(post) {
+    const response = await fetch(`http://${SERVER_IP_PORT_TUPLE}/server/createThread?post_id=${post}`,
+        { method: 'GET' }
+    );
+    const data = await response.json();
+    return data;
+}
+
 export async function deleteThread(username, hash, post) {
     const message = JSON.stringify({ user: username, pwHash: hash, postData: post });
     const response = await fetch(`http://${SERVER_IP_PORT_TUPLE}/server/deleteThread?data=${message}`, 
