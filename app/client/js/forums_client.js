de1777fbfdfed5f8532cddeca548cd2ebde49554
ssamsel <ssamsel@umass.edu>
2023-04-24 17:31:21 -0400

    Minor fixed to prior commit

--- app/client/js/forums_client.js
+++ app/client/js/forums_client.js
@@ -19,7 +19,21 @@ if (username !== null) {
 
 // Get the threads and put them in the page
 const dumpedThreads = await crud.dumpThreads();
-let threads_html = "";
+let threads_html = dumpedThreads.length !== 0 ? "" : `<div class="container-fluid forum bottom-row">
+        <div class="w-50"></div>
+        <div class="col-sm row-item">
+          <div class="circle-div"></div>
+        </div>
+        <div class="col-sm row-item">
+          <div class="circle-div"></div>
+        </div>
+        <div class="col-sm row-item">
+          <p class="small-text"></p>
+          <div class="author-div">
+          </div>
+        </div>
+      </div>`;
+
 dumpedThreads.forEach((x, idx) => {
     const last = idx === dumpedThreads.length - 1 ? "bottom-row" : "";
     const template = `<div class="container-fluid forum ${last}">
