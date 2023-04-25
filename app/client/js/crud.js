dafc2de47843df4be2ffb2fcc1b5a3036352bebb
Shymon Samsel <ssamsel@umass.edu>
2023-04-25 19:48:11 -0400

    Removed client-side password hashing as it was stupid and actually made security worse. It also would not work without https so deploying this website would break it

--- app/client/js/crud.js
+++ app/client/js/crud.js
@@ -1,32 +1,30 @@
-export const SERVER_NAME = window.location.origin;
+export const ORIGIN = window.location.origin;
 
-
-
-export async function logIn(username, hash) {
-    const response = await fetch(`${SERVER_NAME}/server/login?${username}=${hash}`,
+export async function logIn(username, password) {
+    const response = await fetch(`${ORIGIN}/server/login?${username}=${password}`,
         { method: 'POST' }
     );
     const data = await response.json();
     return data;
 }
 
-export async function createAccount(username, hash) {
-    const response = await fetch(`${SERVER_NAME}/server/createAccount?${username}=${hash}`,
+export async function createAccount(username, password) {
+    const response = await fetch(`${ORIGIN}/server/createAccount?${username}=${password}`,
         { method: 'PUT' }
     );
     const data = await response.json();
     return data;
 }
 
-export async function logOut(username, hash) {
-    const response = await fetch(`${SERVER_NAME}/server/logout?${username}=${hash}`,
+export async function logOut(username, password) {
+    const response = await fetch(`${ORIGIN}/server/logout?${username}=${password}`,
         { method: 'POST' }
     );
 }
 
-export async function createThread(username, hash, post, image, hasImage) {
-    const message = JSON.stringify({ user: username, pwHash: hash, postData: post, hasImage: hasImage});
-    const response = await fetch(`${SERVER_NAME}/server/createThread?data=${message}`,
+export async function createThread(username, password, post, image, hasImage) {
+    const message = JSON.stringify({ user: username, pw: password, postData: post, hasImage: hasImage});
+    const response = await fetch(`${ORIGIN}/server/createThread?data=${message}`,
         { method: 'POST', body: image }
     );
     const data = await response.json();
@@ -34,15 +32,15 @@ export async function createThread(username, hash, post, image, hasImage) {
 }
 
 export async function getThread(post) {
-    const response = await fetch(`${SERVER_NAME}/server/getThread?post_id=${post}`,
+    const response = await fetch(`${ORIGIN}/server/getThread?post_id=${post}`,
         { method: 'GET' }
     );
     const data = await response.json();
     return data;
 }
 
-export async function createComment(post_parent, post_id, parent_id, username, pwHash, text) {
-    const response = await fetch(`${SERVER_NAME}/server/createComment?post_id=${post_id}&post_parent=${post_parent}&parent_id=${parent_id}&username=${username}&pwHash=${pwHash}&text=${text}`,
+export async function createComment(post_parent, post_id, parent_id, username, password, text) {
+    const response = await fetch(`${ORIGIN}/server/createComment?post_id=${post_id}&post_parent=${post_parent}&parent_id=${parent_id}&username=${username}&pw=${password}&text=${text}`,
         { method: 'POST' }
     );
     const data = await response.json();
@@ -50,16 +48,16 @@ export async function createComment(post_parent, post_id, parent_id, username, p
 }
 
 export async function getComments(post) {
-    const response = await fetch(`${SERVER_NAME}/server/getComments?post_id=${post}`,
+    const response = await fetch(`${ORIGIN}/server/getComments?post_id=${post}`,
         { method: 'GET' }
     );
     const data = await response.json();
     return data;
 }
 
-export async function deleteThread(username, hash, post) {
-    const message = JSON.stringify({ user: username, pwHash: hash, postData: post });
-    const response = await fetch(`${SERVER_NAME}/server/deleteThread?data=${message}`, 
+export async function deleteThread(username, password, post) {
+    const message = JSON.stringify({ user: username, pw: password, postData: post });
+    const response = await fetch(`${ORIGIN}/server/deleteThread?data=${message}`, 
         { method: 'POST' }
     );
     const data = await response.json();
@@ -67,7 +65,7 @@ export async function deleteThread(username, hash, post) {
 }
 
 export async function dumpThreads(){
-    const response = await fetch(`${SERVER_NAME}/server/dumpThreads`, 
+    const response = await fetch(`${ORIGIN}/server/dumpThreads`, 
     {method: 'GET'}
     );
     const data = await response.json();
@@ -75,7 +73,7 @@ export async function dumpThreads(){
 }
 
 export async function updateLikeCount(comment) {
-    const response = await fetch(`${SERVER_NAME}/server/updateLikeCount?comment=${comment}`,
+    const response = await fetch(`${ORIGIN}/server/updateLikeCount?comment=${comment}`,
       { method: 'POST' }
     );
     const data = await response.json();
