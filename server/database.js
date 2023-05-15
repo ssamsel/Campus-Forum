e8f453f4b1909c42240181e28a02888eec60a40a
Shymon Samsel <ssamsel@umass.edu>
2023-05-15 00:20:16 -0400

    fixed an accidental deletion

--- server/database.js
+++ server/database.js
@@ -1,42 +1,105 @@
-import PouchDB from "pouchdb";
 import * as timeUtils from "./time.js";
 import { createHash } from "node:crypto";
-import path from "path";
+import pg from "pg";
+
+const pg_options = {
+  user: "mmteam",
+  password: process.env.PGPASSWORD,
+  host: "127.0.0.1",
+  database: "mm",
+  port: 5432,
+};
+const pool = new pg.Pool(pg_options);
+const client = new pg.Client(pg_options);
+
+const account_table = `
+CREATE TABLE IF NOT EXISTS accounts (
+  username text,
+  hash text,
+  likes text[]
+);`;
+const thread_table = `
+CREATE TABLE IF NOT EXISTS threads (
+  title text,
+  author text,
+  body text,
+  time bigint,
+  images integer,
+  image_path text,
+  posts integer,
+  comments text[]
+);`;
+const comment_table = `
+CREATE TABLE IF NOT EXISTS comments (
+  comment_id text,
+  author text,
+  comment_body text,
+  time bigint,
+  likes integer,
+  children text[]
+);`;
+
+try {
+  await client.connect();
+} catch (err) {
+  console.error("Failed to connect to database!");
+  console.error("Did you start the daemon and create 'mm'?");
+  console.error(err);
+  process.exit(1);
+}
+
+try {
+  await client.query(account_table);
+  await client.query(thread_table);
+  await client.query(comment_table);
+} catch (err) {
+  console.error("Error Creating Tables");
+  console.error(err);
+  process.exit(1);
+}
+client.end();
 
 function hashPassword(password) {
   return createHash("sha256").update(password).digest("base64");
 }
 
-class AccountDB {
-  constructor() {
-    this.db = new PouchDB(path.join(process.env.PWD, "/db/accounts"));
-  }
+class AccountTable {
+  constructor() {}
 
   // Returns true iff an account with username exists
   async exists(username) {
-    const docs = await this.db.allDocs({ include_docs: true });
-    return docs.rows.some((x) => x.id === username);
+    const users = await pool.query(
+      `SELECT username FROM accounts WHERE username = $1;`,
+      [username]
+    );
+    return users.rows.length != 0;
   }
 
   // Puts a new user into db
   create(username, password) {
-    this.db.put({
-      _id: username,
-      pwHash: hashPassword(password),
-      likes: [],
-    });
+    pool.query(
+      `INSERT INTO accounts VALUES ('${username}','${hashPassword(
+        password
+      )}','{}');`
+    );
   }
 
   // Checks password of username in db
   async checkPassword(username, password) {
-    const account = await this.db.get(username);
-    return account.pwHash === hashPassword(password);
+    const account = await pool.query(
+      `SELECT username,hash FROM accounts WHERE username = '${username}';`
+    );
+    return account.rows[0].hash === hashPassword(password);
   }
 
   // Toggles whether a user liked a comment or not, and returns [1 || -1], used to update like count in commentDB
   async toggleCommentLike(username, comment) {
     let ret = 0;
-    const account = await this.db.get(username);
+    const account = (
+      await pool.query(
+        `SELECT username,likes FROM accounts WHERE username = '${username}';`
+      )
+    ).rows[0];
     if (account.likes.some((x) => x === comment)) {
       ret = -1;
       account.likes.splice(account.likes.indexOf(comment), 1);
@@ -44,77 +107,87 @@ class AccountDB {
       ret = 1;
       account.likes.push(comment);
     }
-    this.db.put(account, { _rev: account.rev, force: true });
+    pool.query(`UPDATE accounts SET likes = $1 WHERE username = $2;`, [
+      account.likes,
+      username,
+    ]);
     return ret;
   }
 }
 
-class ThreadDB {
-  constructor() {
-    this.db = new PouchDB(path.join(process.env.PWD, "/db/threads"));
-  }
+class ThreadTable {
+  constructor() {}
 
   async exists(title) {
-    const docs = await this.db.allDocs({ include_docs: true });
-    return docs.rows.some((x) => x.id === title);
+    const docs = await pool.query(
+      `SELECT title FROM threads WHERE title = $1`,
+      [title]
+    );
+    return docs.rows.length != 0;
   }
 
   create(author, title, text, imagePath) {
-    this.db.put({
-      _id: title,
-      author: author,
-      body: text,
-      time: Date.now(),
-      images: imagePath ? 1 : 0,
-      imagePath: imagePath,
-      posts: 1,
-      comments: [],
-    });
+    pool.query(`INSERT INTO threads VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`, [
+      title,
+      author,
+      text,
+      Date.now(),
+      0,
+      imagePath,
+      1,
+      [],
+    ]);
   }
 
   async updateTimeStamp(title) {
-    const thread = await this.db.get(title);
-    thread.time = Date.now();
-    this.db.put(thread, { _rev: thread._rev, force: true });
+    await pool.query(`UPDATE threads SET time = $1 WHERE title = $2;`, [
+      Date.now(),
+      title,
+    ]);
   }
 
   // Increments post count and returns the new value
   async incrementPostCount(title) {
-    const thread = await this.db.get(title);
-    thread.posts++;
-    this.db.put(thread, { _rev: thread._rev, force: true });
-    return thread.posts;
+    await pool.query(
+      `UPDATE threads SET posts = posts + 1 WHERE title = $1;`,
+      [title]
+    );
+    return (
+      await pool.query(`SELECT posts FROM threads WHERE title = $1;`, [title])
+    ).rows[0].posts;
   }
 
-  async addTopLevelComment(title, comment_id) {
-    const thread = await this.db.get(title);
-    thread.comments.push(comment_id);
-    this.db.put(thread, { _rev: thread._rev, force: true });
+  addTopLevelComment(title, comment_id) {
+    pool.query(
+      `UPDATE threads SET comments = comments || $1 WHERE title = $2;`,
+      [[comment_id], title]
+    );
   }
 
   async get(title) {
-    const thread = await this.db.get(title);
+    const thread = (
+      await pool.query(`SELECT * FROM threads WHERE title = $1`, [title])
+    ).rows[0];
     return {
-      title: thread._id,
+      title: thread.title,
       author: thread.author,
       body: thread.body,
       time: thread.time,
       images: thread.images,
-      imagePath: thread.imagePath,
+      imagePath: thread.image_path ? thread.image_path : undefined,
       posts: thread.posts,
       comments: thread.comments,
     };
   }
 
-  async delete(title) {
-    await this.db.remove(await this.db.get(title));
+  delete(title) {
+    pool.query(`DELETE FROM threads WHERE title = $1`, [title]);
   }
 
   async dump(amount, page) {
-    const allDocs = await this.db.allDocs({ include_docs: true });
-    let threads = [];
-    allDocs.rows.forEach((x) => threads.push(x.doc));
-    threads.sort(timeUtils.compare);
+    let threads = (
+      await pool.query(`SELECT * FROM threads ORDER BY time DESC;`)
+    ).rows;
 
     // Get the page requested, if applicable
     if (amount !== undefined && page != undefined && amount !== "All") {
@@ -126,7 +199,7 @@ class ThreadDB {
     return threads.map((x) => {
       return {
         author: x.author,
-        title: x._id,
+        title: x.title,
         body: x.body,
         time: timeUtils.convertToRecencyString(x.time),
         images: x.images,
@@ -136,35 +209,35 @@ class ThreadDB {
   }
 
   async total() {
-    const allDocs = await this.db.allDocs({ include_docs: true });
-    return allDocs.total_rows;
+    return (await pool.query(`SELECT title FROM threads;`)).rows.length;
   }
 }
 
-class CommentDB {
-  constructor() {
-    this.db = new PouchDB(path.join(process.env.PWD, "/db/comments"));
-  }
+class CommentTable {
+  constructor() {}
 
-  async addChild(parent_id, id) {
-    const parent = await this.db.get(parent_id);
-    parent.children.push(id);
-    this.db.put(parent), { _rev: parent._rev, force: true };
+  addChild(parent_id, id) {
+    pool.query(`UPDATE comments SET children = children || $1 WHERE comment_id = $2;`, [
+      [id],
+      parent_id,
+    ]);
   }
 
   create(id, author, text) {
-    this.db.put({
-      _id: id,
-      author: author,
-      comment_body: text,
-      time: Date.now(),
-      likes: 0,
-      children: [],
-    });
+    pool.query(`INSERT INTO comments VALUES ($1, $2, $3, $4, $5, $6);`, [
+      id,
+      author,
+      text,
+      Date.now(),
+      0,
+      [],
+    ]);
   }
 
   async load(id) {
-    const comment = await this.db.get(id);
+    const comment = (
+      await pool.query(`SELECT * FROM comments WHERE comment_id = $1;`, [id])
+    ).rows[0];
     for (let i = 0; i < comment.children.length; i++) {
       const child = await this.load(comment.children[i]);
       comment.children[i] = child;
@@ -172,7 +245,8 @@ class CommentDB {
     return comment;
   }
 
-  async deleteAllFromThread(title) {
+  deleteAllFromThread(title) {
+    /*
     const comments = await this.db.allDocs({ include_docs: true });
     const db = this.db;
     comments.rows.forEach(async (comment) => {
@@ -180,30 +254,39 @@ class CommentDB {
       if (re.test(comment.id)) {
         await db.remove(await db.get(comment.id));
       }
-    });
+    });*/
+
+    pool.query(`DELETE FROM comments WHERE comment_id ~ $1;`, [
+      `^[0-9]+-${title}$`,
+    ]);
   }
 
   // Adds amount to comment_id's like count
-  async changeLikeCount(comment_id, amount) {
-    const comment = await this.db.get(comment_id);
-    comment.likes += amount;
-    await this.db.put(comment, { _rev: comment._rev, force: true });
+  changeLikeCount(comment_id, amount) {
+    pool.query(
+      `UPDATE comments SET likes = likes + $1 WHERE comment_id = $2;`,
+      [amount, comment_id]
+    );
   }
 
-  async getAuthor(comment_id){
-    const comment = await this.db.get(comment_id);
-    return comment.author;
+  async getAuthor(comment_id) {
+    return (
+      await pool.query(`SELECT author FROM comments WHERE comment_id = $1;`, [
+        comment_id,
+      ])
+    ).rows[0].author;
   }
 
-  async changeText(comment_id, text){
-    const comment = await this.db.get(comment_id);
-    comment.comment_body = text;
-    await this.db.put(comment, {_rev: comment._rev, force: true});
+  changeText(comment_id, text) {
+    pool.query(`UPDATE comments SET comment_body = $1 WHERE comment_id = $2;`, [
+      text,
+      comment_id,
+    ]);
   }
 }
 
-const accounts = new AccountDB();
-const threads = new ThreadDB();
-const comments = new CommentDB();
+const accounts = new AccountTable();
+const threads = new ThreadTable();
+const comments = new CommentTable();
 
 export { accounts, threads, comments };
